75f91cb7380db921286927396ea983eb7c76461c
Abrackadabrr <iliynea2002@ya.ru>
2021-03-02 16:57:07 +0300

    изменил консоль класс

--- Console_draw.cpp
+++ Console_draw.cpp
@@ -9,21 +9,28 @@ Console_draw::Console_draw()
 
     while (getline(in, s))
     {
-        auto pos = s.find(" ");
+        auto pos = s.find("|");
         if (pos != string::npos)
         {
             string s1 = s.substr(0,pos);
             string s2 = s.substr(pos+1);
+            for (int i = 0; i < s2.length(); i++)
+            {
+                if (s2[i] == 'n') s2[i] = '\n';
+            }
             massages.push_back(s1, s2);
         }
     }
     in.close();
 
-    start[0] = "hello";
-    start[1] = "hello";
-    start[2] = "hello";
-    start[3] = "hello";
-    start[4] = "hello";
+    start[0] = "приветствие";
+    start[1] = "правила";
+    start[2] = "начать";
+
+    players[0] = "выбор игрока";
+    players[1] = "воин";
+    players[2] = "волшебник";
+
 }
 
 void Console_draw::print(string name)
@@ -33,7 +40,14 @@ void Console_draw::print(string name)
 
 void Console_draw::set_hello_hat()
 {
-    for (int i = 0; i < 4; i++)
-        cout << this->massages.find("hello")<<endl;
+    for(int i = 0; i < 3; i++){
+        cout << this->massages.find(start[i])<<endl;
+    }
+}
 
+void Console_draw::set_player_type()
+{
+    for(int i = 0; i < 3; i++){
+        cout << this->massages.find(players[i])<<endl;
+    }
 }
--- Console_draw.h
+++ Console_draw.h
@@ -5,10 +5,11 @@ using namespace std;
 class Console_draw {
 private:
     listt massages;
-    string start[5];
+    string start[3];
+    string players[3];
 public:
-
     Console_draw();
     void print(string name);
     void set_hello_hat();
+    void set_player_type();
 };
