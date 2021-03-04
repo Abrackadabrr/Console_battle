018720f0dc589a70f211cd3d6177c693cb66a5af
Karim Vafin <karim.vafin@mail.ru>
2021-03-04 19:33:05 +0300

    добавил класс dragon: public monster

---
+++ dragon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Карим Вафин on 03.03.2021.
+//
+
+#include "dragon.h"
---
+++ dragon.h
@@ -0,0 +1,16 @@
+//
+// Created by Карим Вафин on 03.03.2021.
+//
+
+#ifndef CONSOLE_BATTLE_DRAGON_H
+#define CONSOLE_BATTLE_DRAGON_H
+#include "monster.h"
+
+class dragon: public monster
+{
+public:
+    
+};
+
+
+#endif //CONSOLE_BATTLE_DRAGON_H
